4214447ae7b54f1bca51e96e28d6d59c3351bfed
Alexander Morogov <42555001+krankenbro@users.noreply.github.com>
2025-02-03 12:11:48 +0100

    VCI-987: Update dependencies (#1)
    * VCI-987: Update dependencies
    
    * Update module-ci.yml

--- src/VirtoCommerce.GithubOAuth.Core/Models/GithubOAuthOptions.cs
+++ src/VirtoCommerce.GithubOAuth.Core/Models/GithubOAuthOptions.cs
@@ -2,13 +2,14 @@ namespace VirtoCommerce.GithubOAuth.Core.Models
 {
     public class GithubOAuthOptions
     {
+        public const string SectionName = "GithubOAuth";
         public bool Enabled { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
         public string RedirectUrl { get; set; }
-        public string DefaultUserType { get; set; }
-        public string[] DefaultUserRoles { get; set; } = new[] { "Manager" };
-        public string[] Scopes { get; set; } = new[] { "user:email" };
+        public string DefaultUserType { get; set; } = "Manager";
+        public string[] DefaultUserRoles { get; set; }
+        public string[] Scopes { get; set; }
         public string AuthenticationType { get; set; }
     }
 }
